813005f1686c092f632a595d615c4d0762b640f8
zqy1018 <zqy1018@sjtu.edu.cn>
2023-12-24 14:23:23 +0800

    disable degenerated clock for now

--- extraction/extract_driver.v
+++ extraction/extract_driver.v
@@ -1,14 +1,12 @@
-From Coq Require Import List Bool Lia ssrbool PeanoNat Sorting RelationClasses.
-From Coq Require ssreflect.
-From arboreta.clocks Require Export treeclock degenerated_treeclock.
-Import ssreflect.SsrSyntax.
+From Coq Require Import List Bool Lia PeanoNat Sorting RelationClasses.
+From arboreta.clocks Require Export treeclock.
 
 From Coq Require Import Extraction.
 Extraction Language OCaml.
 
 Extract Inductive sumbool => "bool" [ "true" "false" ].
 Extract Inductive bool => "bool" [ "true" "false" ].
-Extract Constant is_left => "(fun x -> x)".
+(* Extract Constant is_left => "(fun x -> x)". *)
 Extract Inductive list => "list" [ "[]" "( :: )" ].
 Extract Inductive prod => "(*)"  [ "(,)" ].
 Extract Inductive option => "option" [ "Some" "None" ].
@@ -21,5 +19,4 @@ Extract Constant PeanoNat.Nat.add => "( + )".
     Currently do not know why and there is no related report in Iris/stdpp/issues ...
     will investigate it later. For now, ignore this
 *)
-Extraction "extraction/lib/tcimpl.ml" tc_flatten tc_rootinfo tc_init tc_incr
-  tc_eraseaclk tc_join tc_join_degen.
+Extraction "extraction/lib/tcimpl.ml" tr_flatten tr_rootinfo tc_init tc_incr tc_join.
